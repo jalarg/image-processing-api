da58200bcd72bb050046d8d998dd766e8250eb42
jalarg <jlema1990@gmail.com>
2025-03-02 14:32:26 -0300

    refactor: add redis set and get cache in GET request

--- src/application/use-cases/get-task.use-case.ts
+++ src/application/use-cases/get-task.use-case.ts
@@ -1,6 +1,7 @@
 import { TaskRepository } from "../../domain/task.repository";
 import { filteredTask } from "../../domain/task.entity";
 import { AppError } from "../../infrastructure/middlewares/errorHandler";
+import { getCache, setCache } from "../../infrastructure/redis/cache";
 
 export class GetTaskUseCase {
   constructor(private readonly taskRepository: TaskRepository) {}
@@ -9,10 +10,20 @@ export class GetTaskUseCase {
     if (!taskId) {
       throw new AppError("Task ID is required", 400);
     }
+    // Check cache
+    const cacheKey = `cache:task:${taskId}`;
+    const cachedTask = await getCache(cacheKey);
+    if (cachedTask) {
+      console.log("Servido desde Redis");
+      return cachedTask;
+    }
+    // Fetch task from mongoDB database
     const task = await this.taskRepository.optimizedFindById(taskId);
     if (!task) {
       throw new AppError("Task not found", 404);
     }
+    // Set cache with TTL 30min
+    await setCache(cacheKey, task, 1800);
     return task;
   }
 }
