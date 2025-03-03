97ec48c42d71675909dd53bb3be311d2dadf570b
jalarg <jlema1990@gmail.com>
2025-03-03 00:38:13 -0300

    refactor: fix error, wrong import in file, move logic to image service

--- src/domain/services/ImageProcessingService.ts
+++ src/domain/services/ImageProcessingService.ts
@@ -5,16 +5,19 @@ import { TaskRepository } from "../repositories/task.repository";
 export class ImageProcessingService {
   constructor(private readonly taskRepository: TaskRepository) {}
 
-  async process(task: Task): Promise<any> {
+  async process(task: Task): Promise<void> {
+    if (!task || !task._id) {
+      throw new Error("Task ID is required for image processing.");
+    }
+    console.log(`Processing image for task: ${task._id}`);
     try {
-      if (!task._id) {
-        throw new Error("Task ID is required for image processing.");
-      }
-      return await processImage(task._id, this.taskRepository);
+      const processedImages = await processImage(task.originalPath);
+      await this.taskRepository.completeTask(task._id, processedImages);
     } catch (error) {
+      console.error(`Error processing task ${task._id}:`, error);
       task.markAsFailed();
       await this.taskRepository.save(task);
-      throw new Error(`Image processing failed for task ${task._id}: ${error}`);
+      throw new Error("Processing error");
     }
   }
 }
