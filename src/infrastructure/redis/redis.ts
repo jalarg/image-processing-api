9a1f09d09dbd2308a49f1fad2fc3228a7f04d9e6
jalarg <jlema1990@gmail.com>
2025-03-19 20:59:59 -0300

    refactor: redis config to use redis stack

--- src/infrastructure/redis/redis.ts
+++ src/infrastructure/redis/redis.ts
@@ -1,9 +1,9 @@
 import Redis from "ioredis";
 
 const redisClient = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
+  host: process.env.REDIS_HOST || "image-processing-api-redis-stack-1",
   port: parseInt(process.env.REDIS_PORT || "6379"),
-  db: parseInt(process.env.REDIS_DB || "0"),
+  db: parseInt(process.env.REDIS_DB || "1"),
   maxRetriesPerRequest: null,
 });
 
