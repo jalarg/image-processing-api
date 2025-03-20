401203e42736ac00df16b9e5cf2a3db869c86725
jalarg <jlema1990@gmail.com>
2025-03-20 06:44:05 -0300

    fix: integration test, mock redis and include task cache and taskQueue service

--- tests/integration/task.routes.spec.ts
+++ tests/integration/task.routes.spec.ts
@@ -2,9 +2,16 @@ import request from "supertest";
 import express from "express";
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
-import taskRoutes from "../../src/infrastructure/routes/task.routes";
 import { errorMiddleware } from "../../src/infrastructure/middlewares/errorHandler";
-import { describe, it, expect, beforeAll, afterAll, beforeEach } from "vitest";
+import {
+  vi,
+  describe,
+  it,
+  expect,
+  beforeAll,
+  afterAll,
+  beforeEach,
+} from "vitest";
 import { TaskRepositoryMongo } from "../../src/infrastructure/repositories/task.repository.mongo";
 import { ProcessImageUseCase } from "../../src/application/use-cases/processImage.use-case";
 import { taskQueue } from "../../src/infrastructure/queues/taskQueue";
@@ -12,10 +19,27 @@ import { ImageProcessingService } from "../../src/infrastructure/services/ImageP
 import { Worker } from "bullmq";
 import { processImage } from "../../src/helpers/processImage";
 import { redisClient } from "../../src/infrastructure/redis/redis";
+import createRouter from "../../src/infrastructure/routes/index.routes";
+import { TaskCacheService } from "../../src/infrastructure/services/TaskCacheService";
+import { TaskQueueService } from "../../src/infrastructure/services/TaskQueueService";
+
+// mock redis
+vi.mock("../../src/infrastructure/redis/redis", () => {
+  return {
+    redisClient: {
+      get: vi.fn().mockResolvedValue(null),
+      set: vi.fn().mockResolvedValue("OK"),
+      del: vi.fn().mockResolvedValue(1),
+      quit: vi.fn().mockResolvedValue(null),
+    },
+  };
+});
 
 // Set up Express app
 const app = express();
 const taskRepository = new TaskRepositoryMongo();
+const taskCacheService = new TaskCacheService();
+const taskQueueService = new TaskQueueService();
 const imageProcessingService = new ImageProcessingService(
   taskRepository,
   processImage
@@ -26,7 +50,10 @@ const processImageUseCase = new ProcessImageUseCase(
 );
 
 app.use(express.json());
-app.use("/tasks", taskRoutes(taskRepository));
+app.use(
+  "/api",
+  createRouter(taskRepository, taskCacheService, taskQueueService)
+);
 app.use(errorMiddleware);
 
 describe("Task Routes Integration Tests", () => {
@@ -64,7 +91,7 @@ describe("Task Routes Integration Tests", () => {
   });
 
   it("should create a new task and verify it completes", async () => {
-    const response = await request(app).post("/tasks").send({
+    const response = await request(app).post("/api/tasks").send({
       originalPath:
         "https://img01.ztat.net/article/spp-media-p1/9a482a65092d35a3b3d966aac611fa35/54d38262b49c4c89a61dfe25c8a73dc1.jpg?imwidth=1800",
     });
@@ -88,7 +115,7 @@ describe("Task Routes Integration Tests", () => {
   });
 
   it("should create a new task and validate pending state", async () => {
-    const response = await request(app).post("/tasks").send({
+    const response = await request(app).post("/api/tasks").send({
       originalPath:
         "https://img01.ztat.net/article/spp-media-p1/9a482a65092d35a3b3d966aac611fa35/54d38262b49c4c89a61dfe25c8a73dc1.jpg?imwidth=1800",
     });
@@ -100,21 +127,21 @@ describe("Task Routes Integration Tests", () => {
 
     const taskId = response.body.taskId;
 
-    const pendingResponse = await request(app).get(`/tasks/${taskId}`);
+    const pendingResponse = await request(app).get(`/api/tasks/${taskId}`);
     expect(pendingResponse.status).toBe(200);
     expect(pendingResponse.body).toHaveProperty("status", "pending");
     expect(pendingResponse.body).toHaveProperty("price");
   });
 
   it("should return an error if no originalPath is provided", async () => {
-    const response = await request(app).post("/tasks").send({});
+    const response = await request(app).post("/api/tasks").send({});
     expect(response.status).toBe(400);
     expect(response.body).toHaveProperty("message", "originalPath is required");
   });
 
   it("should return 404 for a non-existent taskId", async () => {
     const errorTaskId = "nonexistenttaskid";
-    const response = await request(app).get(`/tasks/${errorTaskId}`);
+    const response = await request(app).get(`/api/tasks/${errorTaskId}`);
     expect(response.status).toBe(404);
   });
 });
